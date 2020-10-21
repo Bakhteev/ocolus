58c16c756c4c06b46e2d0a461f0c5eb422cd1682
Bogdan-create <bogdan-shigaev@mail.ru>
2020-10-21 18:37:09 +0600

    deleted time

--- js/index.js
+++ js/index.js
@@ -1,8 +1,7 @@
 import header from './header/index.js'
-import { imgWrapper, time } from './photoGenerator/index.js'
+import { imgWrapper} from './photoGenerator/index.js'
 
 
 const root = document.getElementById('root')
 root.appendChild(header)
-root.appendChild(time)
 root.appendChild(imgWrapper)
